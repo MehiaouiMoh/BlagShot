cb8620b3c6e4a8329b928bc464e73b878ca03415
MehiaouiMoh <mehiaoui_m@outlook.fr>
2025-09-26 08:49:49 +0200

    Initial Commit

---
+++ BlagShot/BlagShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlagShotApp.swift
+//  BlagShot
+//
+//  Created by admin on 26/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct BlagShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BlagShotTests/BlagShotTests.swift
@@ -0,0 +1,16 @@
+//
+//  BlagShotTests.swift
+//  BlagShotTests
+//
+//  Created by admin on 26/09/2025.
+//
+
+import Testing
+
+struct BlagShotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
